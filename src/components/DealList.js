bdec5007bc84a8a09137fc93eed463b9a3283d1d
Toru <t.furuya825@gmail.com>
2021-03-24 14:35:30 +0900

    Using FlatList with API data

--- src/components/DealList.js
+++ src/components/DealList.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
 
 class DealList extends React.Component {
+  static propTypes = {
+    deals: PropTypes.array.isRequired,
+  }
   render() {
     return (
-      <View>
-        <Text>Details...</Text>
+      <View style={styles.list}>
+        <FlatList
+          data={this.props.deals}
+          renderItem={({item}) => <Text>{item.title}</Text>}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  list: {
+    backgroundColor: '#eee',
+    flex: 1,
+    width: '100%',
+  }
+});
 
 export default DealList;
